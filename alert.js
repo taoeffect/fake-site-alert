d02295b3affc8e96f22042f0ff04ab17819198d8
Brian Feldman <brian.feldman42@gmail.com>
2016-11-16 08:14:24 -0500

    text update
    updated banner text to qualify the fake assertion

--- alert.js
+++ alert.js
@@ -4,8 +4,8 @@ var striped_lines_box = document.createElement('div');
 striped_lines_box.style.cssText = "background-image: url(https://i.imgur.com/m5Wjo7h.png); height: 20px;width: 100%;position: fixed; z-index: 99999; opacity: 0.7;";
 
 var inner_text = document.createElement('span');
-inner_text.textContent = "This is a known fake news site!";
-inner_text.style.cssText = "font-family: 'Lucida Console', Monaco, monospace; color: white; margin: auto; background: black; display: block; height: 20px; width: 300px; text-align: center; font-size: 14px;";
+inner_text.textContent = "The information on this site might be false or misleading!";
+inner_text.style.cssText = "font-family: 'Lucida Console', Monaco, monospace; color: white; margin: auto; background: black; display: block; height: 20px; width: 500px; text-align: center; font-size: 14px;";
 
 striped_lines_box.appendChild(inner_text);
 
